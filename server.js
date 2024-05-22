e49001c74b5b0344995cc186cfa8532e49455388
lukSkul <lukskul@elephant.mynetworksettings.com>
2024-05-22 06:18:45 -0700

    Setting up working index.html for gitpages

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const dataFilePath = path.join(__dirname, 'data', 'boats.json');
